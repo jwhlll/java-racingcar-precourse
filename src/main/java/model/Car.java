97cbae72479294f8ed4ebc8c2be0819ccff21796
jwhlll <jwhlll@hanmail.net>
2021-04-30 17:48:53 +0900

    feat: Car 객체 Cars 객체 생성

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void addPosition() {
+        this.position++;
+    }
+}
---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    private List<Car> cars = new ArrayList<>();
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+}

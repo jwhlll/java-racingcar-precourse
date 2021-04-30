bc887d9383206112dc148153e6f0ca121dfd40eb
jwhlll <jwhlll@hanmail.net>
2021-04-30 13:50:53 +0900

    feat: 시도 횟수 입력 View

--- src/main/java/view/RacingCarView.java
+++ src/main/java/view/RacingCarView.java
@@ -9,4 +9,10 @@ public class RacingCarView {
         System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         return sc.nextLine();
     }
+
+    public static int inputTryNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("시도할 횟수는 몇번 인가요?");
+        return sc.nextInt();
+    }
 }

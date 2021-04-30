9f563e85483645e7302e8b14cc9de19193a1f381
jwhlll <jwhlll@hanmail.net>
2021-04-30 19:21:43 +0900

    feat: 자동차 이름 유효성 체크

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,10 +1,17 @@
 package model;
 
+import java.util.Random;
+
 public class Car {
     private String name;
     private int position;
 
+    final static int MAX_RANDOM_LENGTH = 5;
+
     public Car(String name) {
+        if(name.length() > MAX_RANDOM_LENGTH){
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         position = 0;
     }

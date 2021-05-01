2e4c855594ca8f144188f044ea71da8c111ef431
jwhlll <jwhlll@hanmail.net>
2021-05-01 09:21:46 +0900

    feat: 자동차 분리 기능

--- src/main/java/service/RacingCarService.java
+++ src/main/java/service/RacingCarService.java
@@ -1,8 +1,31 @@
 package service;
 
+import model.Car;
+import model.Cars;
+import view.RacingCarView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingCarService {
 
     public void start() {
+        String inputCarsStr = RacingCarView.inputCar();
+        String[] carNameArr = splitCar(inputCarsStr);
+        Cars cars = new Cars(initCars(carNameArr));
+    }
 
+    private String[] splitCar(String inputCarsStr) {
+        return inputCarsStr.split(",");
+    }
+
+    private List<Car> initCars(String[] carNameArr) {
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNameArr) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
     }
 }

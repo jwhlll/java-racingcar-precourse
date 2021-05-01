08c6cd41ca671d67d444c590c649db7606ae9216
jwhlll <jwhlll@hanmail.net>
2021-05-01 09:00:28 +0900

    feat: 전진 및 멈춤 기능   - Random 값 생성   - 4이상 전진 3이하 멈춤

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,12 +1,12 @@
 package model;
 
-import java.util.Random;
-
 public class Car {
     private String name;
     private int position;
 
     final static int MAX_RANDOM_LENGTH = 5;
+    final static int MAX_RANDOM_NUMBER = 9;
+    final static int RUN_CONDITION = 4;
 
     public Car(String name) {
         if(name.length() > MAX_RANDOM_LENGTH){
@@ -24,7 +24,21 @@ public class Car {
         return position;
     }
 
-    public void addPosition() {
-        this.position++;
+    public void run() {
+        int randomValue = randomValue();
+        if(isRun(randomValue)) {
+            this.position++;
+        }
+    }
+
+    private int randomValue() {
+        return (int)(Math.random()*MAX_RANDOM_NUMBER);
+    }
+
+    private boolean isRun(int randomValue) {
+        if(randomValue > RUN_CONDITION) {
+            return false;
+        }
+        return true;
     }
 }

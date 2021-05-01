e17be8bfab49f954af36acd9a35644948d4fa6b7
jwhlll <jwhlll@hanmail.net>
2021-05-01 09:06:25 +0900

    refactor: 자동차 입력 View 오타 수정

--- src/main/java/view/RacingCarView.java
+++ src/main/java/view/RacingCarView.java
@@ -6,13 +6,13 @@ public class RacingCarView {
 
     public static String inputCar() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) ");
         return sc.nextLine();
     }
 
     public static int inputTryNumber() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("시도할 횟수는 몇번 인가요?");
+        System.out.println("시도할 횟수는 몇번 인가요?");
         return sc.nextInt();
     }
 }

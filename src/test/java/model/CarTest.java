52d8e4369a64a437773bd2b96e950e6a00c19e65
jwhlll <jwhlll@hanmail.net>
2021-05-02 10:29:09 +0900

    test: 자동차 이름 테스트

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -19,4 +19,14 @@ class CarTest {
         }
         assertTrue(car.getPosition() <= RUN_COUNT);
     }
+
+    @Test
+    @DisplayName("자동차 이름 5글자 이상 테스트")
+    void  car_name_test() {
+        String carName = "abcdef";
+        assertThatThrownBy(()->{
+            new Car(carName);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5글자 이하만 가능합니다.");
+    }
 }
